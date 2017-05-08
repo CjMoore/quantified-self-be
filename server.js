217f9331245b332c3df725812b65db1d6b713fdc
Charlotte Moore <mooc123@gmail.com>
2017-05-08 14:14:55 -0600

    adds knex, makes foods table

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const logger = require('morgan')
 const bodyParser = require('body-parser')
-
 const app = express()
 
 app.use(bodyParser.json());
+
 app.use(logger('dev'))
 
 app.set('port', process.env.PORT || 3000)
