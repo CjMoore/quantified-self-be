07c4c9fae0e74abf2d99a7678628742dfdbad2e1
Charlotte Moore <mooc123@gmail.com>
2017-05-10 13:50:37 -0600

    added headers for access allow control middleware

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ const MealsController = require('./lib/controllers/meals-controller')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+});
+
 // app.use(logger('dev'))
 
 app.set('port', process.env.PORT || 3000)
