6ed694afb2913001ec6e7cda91e5ccfd2202df75
Charlotte Moore <mooc123@gmail.com>
2017-05-11 18:29:55 -0600

    get /api/v1/diaries/meals returns diary id if no meals are associated with diary

--- test/server-test.js
+++ test/server-test.js
@@ -271,11 +271,6 @@ describe('Server', () => {
 
     it('returns data about meals associated with that date and the food for those meals', (done) => {
 
-      // const diaryDate = {
-      //   date: '2017-05-09'
-      // }
-      // {form: diaryDate}
-
       this.request.get('/api/v1/diaries/meals?date=2017-05-09',(error, response) => {
         const meals = JSON.parse(response.body)
         const foods = meals[0].foods
@@ -294,7 +289,6 @@ describe('Server', () => {
     it('creates and returns a new diary if diary does not exist', (done) => {
 
       const diaryDateMake = { date: '2017-05-20'}
-      // {form: diaryDateMake}
       this.request.get('/api/v1/diaries/meals?date=2017-05-20', (error, response) =>{
         const diary = JSON.parse(response.body)
 
