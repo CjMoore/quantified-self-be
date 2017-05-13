7712c695780f3a6558d83b2a01bce3ce3b5865b9
Charlotte Moore <mooc123@gmail.com>
2017-05-13 15:21:10 -0600

    add meal to today

--- db/seeds/production/meals.js
+++ db/seeds/production/meals.js
@@ -10,6 +10,7 @@ exports.seed = function(knex, Promise) {
       knex.raw('INSERT INTO meals (name, food_id, diary_id, created_at) VALUES(?,?,?,?)', ["Lunch",4, 2, new Date ]),
       knex.raw('INSERT INTO meals (name, food_id, diary_id, created_at) VALUES(?,?,?,?)', ["Dinner", 1, 3, new Date ]),
       knex.raw('INSERT INTO meals (name, food_id, diary_id, created_at) VALUES(?,?,?,?)', ["Dinner", 3, 5, new Date ]),
+      knex.raw('INSERT INTO meals (name, food_id, diary_id, created_at) VALUES(?,?,?,?)', ["Dinner", 2, 5, new Date ]),
       knex.raw('INSERT INTO meals (name, food_id, diary_id, created_at) VALUES(?,?,?,?)', ["Snacks", 1, 4, new Date ]),
       knex.raw('INSERT INTO meals (name, food_id, diary_id, created_at) VALUES(?,?,?,?)', ["Lunch", 2, 6, new Date ]),
       knex.raw('INSERT INTO meals (name, food_id, diary_id, created_at) VALUES(?,?,?,?)', ["Lunch", 3, 5, new Date ]),
