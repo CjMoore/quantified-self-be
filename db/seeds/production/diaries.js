a7619c22c40ba4fa0231fb119239efead895b26a
Charlotte Moore <mooc123@gmail.com>
2017-05-13 14:36:09 -0600

    fixed syntax

--- db/seeds/production/diaries.js
+++ db/seeds/production/diaries.js
@@ -5,11 +5,11 @@ exports.seed = function(knex, Promise) {
       knex.raw('INSERT INTO diaries (date, created_at) VALUES(?,?)', [ "2017-05-09", new Date ]),
       knex.raw('INSERT INTO diaries (date, created_at) VALUES(?,?)', [ "2017-05-10", new Date ]),
       knex.raw('INSERT INTO diaries (date, created_at) VALUES(?,?)', [ "2017-05-11", new Date ]),
-      knex.raw('INSERT INTO diaries (date, created_at) VALUES(?,?)', [ "2017-05-12", new Date ]
-      knex.raw('INSERT INTO diaries (date, created_at) VALUES(?,?)', [ "2017-05-13", new Date ]
-      knex.raw('INSERT INTO diaries (date, created_at) VALUES(?,?)', [ "2017-05-14", new Date ]
-      knex.raw('INSERT INTO diaries (date, created_at) VALUES(?,?)', [ "2017-05-15", new Date ]
-      knex.raw('INSERT INTO diaries (date, created_at) VALUES(?,?)', [ "2017-05-16", new Date ]
+      knex.raw('INSERT INTO diaries (date, created_at) VALUES(?,?)', [ "2017-05-12", new Date ]),
+      knex.raw('INSERT INTO diaries (date, created_at) VALUES(?,?)', [ "2017-05-13", new Date ]),
+      knex.raw('INSERT INTO diaries (date, created_at) VALUES(?,?)', [ "2017-05-14", new Date ]),
+      knex.raw('INSERT INTO diaries (date, created_at) VALUES(?,?)', [ "2017-05-15", new Date ]),
+      knex.raw('INSERT INTO diaries (date, created_at) VALUES(?,?)', [ "2017-05-16", new Date ]),
     ])
   })
 };
